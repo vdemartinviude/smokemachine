c740289f908fad0e4d741d1da44b6a97a4fe7772
Vinicius De Martin Viude <vdemartinviude@gmail>
2023-05-19 15:21:02 -0300

    Versão Final˜

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
+
 from time import sleep
 from flask import Flask,request, render_template
 from flask.json import jsonify
 import RPi.GPIO as GPIO
-import socket
 import netifaces
-import os
-from dotenv import load_dotenv
-from sendmsg import sendmessage
+
 
 app = Flask(__name__)
 
@@ -29,25 +27,36 @@ def get_ip_address(interface_name):
 
 
 class PostData:
-    def __init__(self,onoff:int):
+    def __init__(self,onoff):
         self.onoff = onoff
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html',baseUrlFromFlask=f"http://{current_ip}",portFromFlask=port_number)
+    return render_template('index.html',baseUrlFromFlask="http://172.16.8.60",portFromFlask=port_number)
 
-@app.route("/on")
+@app.route("/on2")
 def turnon():
+    GPIO.output(5,GPIO.LOW)
+    respData = { 'msg':'The Led has been turned on by Flask'}
+    return jsonify(respData)
+
+@app.route("/off2")
+def turnoff():
     GPIO.output(5,GPIO.HIGH)
+    respData = { 'msg':'The Led has been turned off by Flask'}
+    return jsonify(respData)
+
+@app.route("/on1")
+def turnon2():
+    GPIO.output(6,GPIO.HIGH)
     respData = { 'msg':'The Machine has been turned on by Flask'}
     return jsonify(respData)
 
-@app.route("/off")
-def turnoff():
-    GPIO.output(5,GPIO.LOW)
-    respData = { 'msg':'The Machine has been turned off by Flask'}
+@app.route("/off1")
+def turnoff2():
+    GPIO.output(6,GPIO.LOW)
+    respData = { 'msg':'The Machine has been turned on by Flask'}
     return jsonify(respData)
-    
 
 @app.route("/toggle", methods=['POST'])
 def handle_toggle():
@@ -66,15 +75,20 @@ def handle_toggle():
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(5,GPIO.OUT)
+    GPIO.setup(6,GPIO.OUT)
     GPIO.output(5,GPIO.LOW)
-    load_dotenv()
-    interface_name = os.getenv("SMOKE_INTERFACE","wlan0")  # Replace with the desired interface name
-    port_number = int(os.getenv("PORT","5000"))
+    sleep(5)
+    GPIO.output(5,GPIO.HIGH)
+    GPIO.output(6,GPIO.LOW)
+    
+
+
+    interface_name = "wlan0"  # Replace with the desired interface name
+    port_number = "5000"
     # Call the function to get the IP address
     
     current_ip = get_ip_address(interface_name)
     print("Current IP address: ",current_ip)
-    sendmessage(f"I'm the shimoo smoke machine and My IP = {current_ip}")
     
     
     app.run(host='0.0.0.0',port=port_number)
